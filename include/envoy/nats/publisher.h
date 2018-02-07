60baa0651061122c534492d43f4d111422a437f9
Tal Nordan <tal.nordan@solo.io>
2018-02-07 15:53:59 -0800

    Document `Publisher::Instance::makeRequest()`
    Add documentation to the pure-virtual member function `makeRequest()`.
    This is intended to clarify that the caller is allowed to pass a `nullptr`
    as the `payload`.

--- include/envoy/nats/publisher.h
+++ include/envoy/nats/publisher.h
@@ -46,6 +46,17 @@ class Instance {
 public:
   virtual ~Instance() {}
 
+  /**
+   * Makes a request.
+   * @param cluster_name supplies the the cluster name.
+   * @param subject supplies the subject.
+   * @param payload supplies the fully buffered payload as buffered by this
+   * filter or previous ones in the filter chain. May be nullptr if nothing has
+   * been buffered.
+   * @param callbacks supplies the request completion callbacks.
+   * @return PublishRequestPtr a handle to the active request or nullptr if the
+   * request could not be made for some reason.
+   */
   virtual PublishRequestPtr makeRequest(const std::string &cluster_name,
                                         const std::string &subject,
                                         const Buffer::Instance *payload,
