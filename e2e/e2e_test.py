dba0103ddab9f74ef1ed109bc1a03a23416e921b
Tal Nordan <tal.nordan@solo.io>
2018-03-20 12:31:22 -0700

    Delete temp file in Python test tear down
    1. Rename `Test` to `ManyRequestsTestCase` to emphasize that it is only
       meant for this single test.
    2. Move temporary file creation to `ManyRequestsTestCase.tearDown()`,
       and close the file in `ManyRequestsTestCase.tearDown()`, so that it
       is deleted.

--- e2e/e2e_test.py
+++ e2e/e2e_test.py
@@ -7,7 +7,16 @@ import tempfile
 import time
 import unittest
 
-class Test(unittest.TestCase):
+class ManyRequestsTestCase(unittest.TestCase):
+  def setUp(self):
+    # A temporary file is used to avoid pipe buffering issues.
+    self.stderr = tempfile.NamedTemporaryFile("rw+", delete=True)
+
+  def tearDown(self):
+    # The file is deleted as soon as it is closed.
+    self.stderr.close()
+    self.stderr = None
+
   def __create_config(self):
     for create_config_path in ("./create_config.sh", "./e2e/create_config.sh"):
       if os.path.isfile(create_config_path):
@@ -29,15 +38,11 @@ class Test(unittest.TestCase):
     subprocess.Popen(["envoy", "-c", "./envoy.yaml", "--log-level", "debug"])
     time.sleep(5)
 
-  @staticmethod
-  def __sub():
-    # A temporary file is used to avoid pipe buffering issues.
-    f = tempfile.NamedTemporaryFile("rw+")
-
+  def __sub(self):
     p = subprocess.Popen(
       ["stan-sub", "-id", "17", "subject1"],
-      stderr=f)
-    return (p, f)
+      stderr=self.stderr)
+    return p
 
   def __make_request(self, payload):
     response = requests.post('http://localhost:10000/post', payload)
@@ -49,11 +54,11 @@ class Test(unittest.TestCase):
     for response in responses:
       self.assertEqual(200, response.status_code)
 
-  def __wait_for_response(self, p, f, data):
+  def __wait_for_response(self, p, data):
     time.sleep(0.1)
     p.terminate()
-    f.seek(0, 0)
-    stderr = f.read()
+    self.stderr.seek(0, 0)
+    stderr = self.stderr.read()
     expected = 'subject:"subject1" data:"%s"' % data
     self.assertTrue(expected in stderr)
 
@@ -61,12 +66,12 @@ class Test(unittest.TestCase):
     self.__create_config()
     self.__start_verbose_nats_streaming_server()
     self.__start_envoy()
-    p, f = self.__sub()
+    p = self.__sub()
     for i in xrange(4):
       payloads = [("solopayload %d %d" % (i, j)) for j in xrange(1024)]
       self.__make_many_requests(payloads)
       time.sleep(0.1)
-    self.__wait_for_response(p, f, "solopayload 3 1023")
+    self.__wait_for_response(p, "solopayload 3 1023")
 
 if __name__ == "__main__":
   logging.basicConfig(level=logging.DEBUG)
